7522c26976abbbc4108e4a22e63f063c792039bb
Simón Grajal <simonvld18@gmail.com>
2026-06-27 16:04:44 +0200

    Initialize device state dictionary with attributes

---
+++ device_state.py
@@ -0,0 +1,26 @@
+device = {
+
+    "name": None,
+
+    "manufacturer": None,
+
+    "intended_use": None,
+
+    "principle": None,
+
+    "active": None,
+
+    "software": None,
+
+    "implantable": None,
+
+    "sterile": None,
+
+    "duration": None,
+
+    "classification": None,
+
+    "rule": None,
+
+    "risk_class": None
+}
